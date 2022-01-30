7e95c6b5cea82463a453823ca8725d5f9b2baf68
anthony dossi <cookieking524@gmail.com>
2022-01-29 23:19:25 -0500

    bye

---
+++ Javascript/mainscripts.js
@@ -0,0 +1,36 @@
+function loadcookie(){
+    document.cookie = 'name="AWSALB"; SameSite=None;Secure;';
+}
+function calming() {
+    var elmnt = document.getElementById("carouselExampleCaptions");
+    elmnt.scrollIntoView();
+}
+
+function skincoat() {
+    var elmnt = document.getElementById("carouselExampleCaptions1");
+    elmnt.scrollIntoView();
+}
+
+function hipjoint() {
+    var elmnt = document.getElementById("carouselExampleCaptions2");
+    elmnt.scrollIntoView();
+}
+
+function digestive() {
+    var elmnt = document.getElementById("carouselExampleCaptions3");
+    elmnt.scrollIntoView();
+}
+
+function dental() {
+    var elmnt = document.getElementById("carouselExampleCaptions4");
+    elmnt.scrollIntoView();
+}
+
+function responsivenav() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "navbar") {
+      x.className += " responsive";
+    } else {
+      x.className = "navbar";
+    }
+  }
